535ae58c2fb24a36f7ba3a58b5974eb527d5781a
Rafael Makaha <rafael.makaha@gmail.com>
2019-04-23 09:41:46 -0300

    Creates Pendings Page.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

---
+++ src/screens/Pendings.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { Paper } from '@material-ui/core';
+import { Answare } from '../components/Answare';
+
+export default class Pendings extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            pendings: [],
+        }
+    }
+
+    componentDidMount(){
+        fetch(
+            'http://0.0.0.0:8000/answares/',
+            {mode: 'no-cors'}
+        ).then(function(response){
+            if(response.ok){
+              return response.json();
+            }
+          }).then(data => {
+              this.setState({ pendings: data })
+          });
+    }
+
+    render(){
+        const pendings = this.state.pendings;
+        return(
+            <Paper >
+                {pendings.map((pending,key) => {
+                    return(
+                        <Answare 
+                        idLimeSurvey={pending.lime_id}
+                        orgao={pending.orgao_nome}
+                        servico={pending.servico_nome}
+                        status='Pendente'
+                        />
+                    )
+                })}
+            </Paper>
+        );
+        
+    }
+}
