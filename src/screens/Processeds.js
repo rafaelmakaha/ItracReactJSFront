adb7d1c50b0a9248bcea236d071a5be619d229f7
Rafael Makaha <rafael.makaha@gmail.com>
2019-04-24 23:05:24 -0300

    Creates Processeds screen.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

---
+++ src/screens/Processeds.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Paper } from '@material-ui/core';
+import { Answare } from '../components/Answare';
+
+export default class Processeds extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            processeds: [],
+        }
+    }
+
+    componentDidMount(){
+        fetch('http://0.0.0.0:8000/answares/'
+        ).then((response) => {
+            if(response.ok){
+                return response.json();
+            }
+        }).then(data => {
+            this.setState({ pendings: data })
+        });
+    }
+
+    render(){
+        const pendings = this.state.pendings;
+        return(
+            <Paper >
+                {pendings.map((pending,key) => {
+                    return(
+                        <Answare 
+                        key={key}
+                        idLimeSurvey={pending.lime_id}
+                        orgao={pending.orgao_nome}
+                        servico={pending.servico_nome}
+                        status='Pendente'
+                        />
+                    )
+                })}
+            </Paper>
+        );
+        
+    }
+}
