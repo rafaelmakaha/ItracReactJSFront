19b69a1cdeaa48054e37de409fea80746ec24b96
Rafael Makaha <rafael.makaha@gmail.com>
2019-04-26 15:21:43 -0300

    Changes OtherRadio component from TextField to SelectField.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

--- src/components/OtherRadio.js
+++ src/components/OtherRadio.js
@@ -1,21 +1,53 @@
 import React, { Component } from "react";
-import { FormControlLabel, Radio, TextField } from "@material-ui/core";
+import { FormControlLabel, 
+  Radio, 
+  TextField,
+  MenuItem,
+} from "@material-ui/core";
 
 export default class OtherRadio extends Component {
   render() {
+    const currencies = [
+      {
+        value: 'USD',
+        label: '$',
+      },
+      {
+        value: 'EUR',
+        label: '€',
+      },
+      {
+        value: 'BTC',
+        label: '฿',
+      },
+      {
+        value: 'JPY',
+        label: '¥',
+      },
+    ];
+
     return (
       <FormControlLabel
         value={this.props.value}
         onChange={this.props.onChange}
         checked={this.props.checked}
-        control={<Radio name="gender" />}
+        control={<Radio />}
         label={
-          <TextField
-            id="standard-bare"
-            placeholder={this.props.placeholder}
-            margin="normal"
-            onChange={this.props.onTextChange}
-          />
+
+        <TextField
+          id="standard-bare"
+          select
+          label="Select"
+          value={this.props.placeholder}
+          onChange={this.props.onTextChange}
+          margin="normal"
+        >
+          {currencies.map(option => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.value}
+            </MenuItem>
+          ))}
+        </TextField>
         }
       />
     );
